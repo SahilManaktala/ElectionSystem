eed76cfa0e58b0a74a8016cc3f7dcc93b8414a75
Sagar Tomar <sagartomar97@gmail.com>
2017-06-15 01:08:53 +0530

    Created Person model and registered it in smart_card/admin.py

--- smart_card/models.py
+++ smart_card/models.py
@@ -31,3 +31,15 @@ class GramPanchayat(models.Model):
 
     def __str__(self):
         return self.tehsil_id + ' ' + self.gram_panchayat_id + ' ' + self.gram_panchayat_name
+
+class Person(models.Model):
+    gram_panchayat_id = models.CharField(max_length = 8)
+    person_id = models.CharField(max_length = 5)
+    first_name = models.CharField(max_length = 20)
+    last_name = models.CharField(max_length = 20)
+    fathers_name = models.CharField(max_length = 50)
+    phone_number = models.CharField(max_length = 10)
+    address = models.CharField(max_length = 1000)
+
+    def __str__(self):
+        return self.gram_panchayat_id + ' ' + self.person_id + ' ' + self.first_name + ' ' + self.last_name
