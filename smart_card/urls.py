f97bb8a774c65ffb5c1b3305812e669fa15a6b30
sagartomar <sagartomar97@gmail.com>
2017-06-15 17:25:57 +0530

    Added sending json data for districts, tehsils and gram panchayats

--- smart_card/urls.py
+++ smart_card/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
     url(r'^register$', views.register, name= 'register'),
+    url(r'^get_districts/$', views.get_districts, name = 'get_districts'),
+    url(r'^get_tehsils/$', views.get_tehsils, name = 'get_tehsils'),
+    url(r'^get_gram_panchayats/$', views.get_gram_panchayats, name = 'get_gram_panchayats')
 ]
--- smart_card/views.py
+++ smart_card/views.py
@@ -1,7 +1,49 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import District, Tehsil, GramPanchayat
 
 def index(request):
     return render(request, 'smart_card/index.html', context=None)
 
 def register(request):
     return render(request, 'smart_card/register.html', context=None)
+
+def get_districts(request):
+    state_id = request.GET.get('state_id', None)
+    data = District.objects.filter(state_id = str(state_id))
+    result = []
+    for row in data:
+        info = {}
+        info['district_id'] = row.district_id
+        info['district_name'] = row.district_name
+        result.append(info)
+    return JsonResponse(result, safe = False)
+
+def get_tehsils(request):
+    country_id = request.GET.get('country_id', None)
+    state_id = request.GET.get('state_id', None)
+    district_id = request.GET.get('district_id', None)
+    parent_id = str(country_id) + str(state_id) + str(district_id)
+    data = Tehsil.objects.filter(district_id = parent_id)
+    result = []
+    for row in data:
+        info = {}
+        info['tehsil_id'] = row.tehsil_id
+        info['tehsil_name'] = row.tehsil_name
+        result.append(info)
+    return JsonResponse(result, safe = False)
+
+def get_gram_panchayats(request):
+    country_id = request.GET.get('country_id', None)
+    state_id = request.GET.get('state_id', None)
+    district_id = request.GET.get('district_id', None)
+    tehsil_id = request.GET.get('tehsil_id', None)
+    parent_id = str(country_id) + str(state_id) + str(district_id) + str(tehsil_id)
+    data = GramPanchayat.objects.filter(tehsil_id = parent_id)
+    result = []
+    for row in data:
+        info = {}
+        info['gram_panchayat_id'] = row.gram_panchayat_id
+        info['gram_panchayat_name'] = row.gram_panchayat_name
+        result.append(info)
+    return JsonResponse(result, safe = False)
