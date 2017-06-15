28fabbc17dd6ccc57d254e7052ebff1a829bd52d
anubhav <anubhav.gupta97@gmail.com>
2017-06-15 12:25:27 +0530

    register.html

--- smart_card/views.py
+++ smart_card/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'smart_card/index.html', context=None)
+
+def register(request):
+    return render(request, 'smart_card/register.html', context=None)
