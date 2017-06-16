693c776c6647beb33d6c3453b67ec956e32c602d
anubhav <anubhav.gupta97@gmail.com>
2017-06-16 10:06:30 +0530

    register.html

--- smart_card/models.py
+++ smart_card/models.py
@@ -39,8 +39,14 @@ class Person(models.Model):
     first_name = models.CharField(max_length = 20)
     last_name = models.CharField(max_length = 20)
     fathers_name = models.CharField(max_length = 50)
+    mothers_name = models.CharField(max_length=50)
+    email= models.CharField(max_length=100)
+    gender = models.CharField(max_length=10)
+    pan = models.CharField(max_length=11)
+
     phone_number = models.CharField(max_length = 10)
     address = models.CharField(max_length = 1000)
+
 
     def __str__(self):
         return self.gram_panchayat_id + ' ' + self.person_id + ' ' + self.first_name + ' ' + self.last_name
