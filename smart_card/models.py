c0abd0e6d5b15edb1b246e9af7501466187d3a0b
Sagar Tomar <sagartomar97@gmail.com>
2017-07-04 13:04:55 +0530

    Changed data in database to adapt to changed models

--- smart_card/models.py
+++ smart_card/models.py
@@ -8,7 +8,7 @@ class Country(models.Model):
         return self.country_id + ' ' + self.country_name
 
 class State(models.Model):
-    country_id = models.CharField(max_length=2)
+    country_id = models.CharField(max_length=2) #CC
     state_id = models.CharField(max_length=2)
     state_name = models.CharField(max_length=100)
 
@@ -16,7 +16,7 @@ class State(models.Model):
         return self.country_id + ' ' + self.state_id + ' ' + self.state_name
 
 class District(models.Model):
-    state_id = models.CharField(max_length = 2)
+    state_id = models.CharField(max_length = 4) #CCSS
     district_id = models.CharField(max_length = 2)
     district_name = models.CharField(max_length = 150)
 
@@ -24,7 +24,7 @@ class District(models.Model):
         return self.state_id + ' ' + self.district_id + ' ' + self.district_name
 
 class Tehsil(models.Model):
-    district_id = models.CharField(max_length = 4) #CSDD
+    district_id = models.CharField(max_length = 6) #CCSSDD
     tehsil_id = models.CharField(max_length = 2)
     tehsil_name = models.CharField(max_length = 150)
 
@@ -32,7 +32,7 @@ class Tehsil(models.Model):
         return self.district_id + ' ' + self.tehsil_id + ' ' + self.tehsil_name
 
 class GramPanchayat(models.Model):
-    tehsil_id = models.CharField(max_length = 6) #CSDDTT
+    tehsil_id = models.CharField(max_length = 8) #CCSSDDTT
     gram_panchayat_id = models.CharField(max_length = 2)
     gram_panchayat_name = models.CharField(max_length = 150)
 
@@ -40,7 +40,7 @@ class GramPanchayat(models.Model):
         return self.tehsil_id + ' ' + self.gram_panchayat_id + ' ' + self.gram_panchayat_name
 
 class Person(models.Model):
-    gram_panchayat_id = models.CharField(max_length = 8)
+    gram_panchayat_id = models.CharField(max_length = 10)
     person_id = models.CharField(max_length = 5)
     password = models.CharField(max_length = 15)
     first_name = models.CharField(max_length = 20)
