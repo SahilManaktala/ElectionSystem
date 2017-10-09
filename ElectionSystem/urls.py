4fee3d2737c64ba26cced2b32c36b4a67d166319
Anubhav Gupta <anubhav.gupta97@gmail.com>
2017-10-09 13:25:13 +0530

    Can be run on a deployment server. Can be accessed from outside.

--- ElectionSystem/urls.py
+++ ElectionSystem/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('smart_card.urls')),
+    url(r'^', include('smart_card.urls')),
     url(r'^smart_card/', include('smart_card.urls')),
     url(r'^admin/', admin.site.urls),
 ]
